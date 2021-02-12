9bba8acbaf958b1e92276cdde12fd6bd81b55362
James Crisafulli <james2048@gmail.com>
2021-02-12 17:11:45 +0000

    MSYS and git download locally, VM friendly.

--- Windows/FFmpeg/build_base.py
+++ Windows/FFmpeg/build_base.py
@@ -4,6 +4,9 @@ import subprocess as sp
 import time
 from path import Path
 import os
+import requests
+import zipfile
+import os
 
 class BuilderBase:
     NUM_CORES = multiprocessing.cpu_count()
@@ -13,39 +16,108 @@ class BuilderBase:
         self.git_address = git_address
         self.linkage = linkage
 
+        self.base_path = Path(__file__).dirname()
+
         self.git_clone_path = Path(f"{self.name}-git")
         self.build_path = Path(f"build-{self.name}-{self.linkage}-git")
         
         self.build_abspath = self.build_path.abspath()
+        self.git_path = self.base_path / "tools/git"
+        self.git_exe_path = (self.git_path / "bin/git.exe").abspath()
 
+        self.msys_path = self.base_path / Path("tools/msys64")
+
+    def fetch_git(self):
+        if not self.git_exe_path.exists():
+            print("Fetching PortableGit archive")
+
+            try:
+                self.git_path.makedirs_p()
+                # TODO put on github files?
+                r = requests.get("https://crisafulli.me/public/JACBuildScripts/git_portable_2.30.1.zip", stream=True)
+                
+                git_zip_path = self.git_path / "git_portable_2.30.1.zip"
+                total = 0
+
+                with open(git_zip_path, "wb") as f:
+                    for c in r.iter_content(chunk_size=64 * 1024):
+                        print(f"Downloaded {total / 1024. / 1024.:.2f} MB / {int(r.headers['Content-Length']) / 1024. / 1024.:.2f} MB", end="\r")
+                        f.write(c)
+                        total += len(c)
+
+                print()
+
+                with zipfile.ZipFile(git_zip_path) as zf:
+                    print("Extracting Git")
+                    zf.extractall(self.git_path)
+                
+                os.remove(git_zip_path)
+
+            except Exception as e:
+                print(str(e))
+                raise
+
+
+    def fetch_msys(self):
+        if not self.msys_path.exists():
+            print("Fetching MSYS64 archive")
+
+            try:
+                self.msys_path.makedirs_p()
+                # TODO put on github files?
+                r = requests.get("https://crisafulli.me/public/JACBuildScripts/msys64.zip", stream=True)
+                
+                msys_zip_path = self.msys_path / "msys64.zip"
+                total = 0
+
+                with open(msys_zip_path, "wb") as f:
+                    for c in r.iter_content(chunk_size=64 * 1024):
+                        print(f"Downloaded {total / 1024. / 1024.:.2f} MB / {int(r.headers['Content-Length']) / 1024. / 1024.:.2f} MB", end="\r")
+                        f.write(c)
+                        total += len(c)
+
+                print()
+
+                with zipfile.ZipFile(msys_zip_path) as zf:
+                    print("Extracting MSYS64")
+                    zf.extractall(self.msys_path)
+                
+                os.remove(msys_zip_path)
+
+            except Exception as e:
+                print(str(e))
+                raise
+                
+        print("Updating MSYS")
+        self.run_in_msys("pacman -Sy --noconfirm", fetch_msys=False)
+        print("Installing compiler")
+        self.run_in_msys("pacman -S --noconfirm --needed base-devel mingw-w64-x86_64-toolchain", fetch_msys=False)
 
     def update_sources(self):
+        self.fetch_git()
+
         if not self.git_clone_path.exists():
-            self.run(f"git clone {self.git_address} {self.git_clone_path}")
+            self.run(f"{self.git_exe_path} clone {self.git_address} {self.git_clone_path}")
             os.chdir(self.git_clone_path)
         else:
             os.chdir(self.git_clone_path)
-            self.run(f"git pull")
+            self.run(f"{self.git_exe_path} pull")
 
-        self.run("git status")
+        self.run(f"{self.git_exe_path} status")
 
         try:
-            self.version = self.run("git describe --tags")
+            self.version = self.run(f"{self.git_exe_path} describe --tags")
             print(self.version)
         except Exception as e:
             print("Can't get tag version")
             self.version = None
-
-        # workaround for directory locking
-        time.sleep(1)
 
         os.chdir("..")
 
     def build(self):
         pass
 
-    @staticmethod
-    def run(cmd, print_output=True):
+    def run(self, cmd, print_output=True):
         if print_output: print("Cmd: " + cmd)
 
         out = sp.check_output(cmd, shell=True).decode("utf-8")
@@ -60,12 +132,14 @@ class BuilderBase:
 
         return out
 
-    @staticmethod
-    def run_in_msys(cmd, working_dir=None, hide_window=False):
+    def run_in_msys(self, cmd, working_dir=None, hide_window=False, fetch_msys=True):
+        if fetch_msys:
+            self.fetch_msys()
+
         if isinstance(cmd, (list, tuple)):
             cmd ="; ".join(cmd)
 
-        if not Path(r"C:\msys64\msys2_shell.cmd").exists():
+        if not (self.msys_path / "msys2_shell.cmd").exists():
             raise Exception("Need msys2 install in default path C:\\msys64\\")
         
         if working_dir:
@@ -74,4 +148,4 @@ class BuilderBase:
         # hide_window_opt = "-w hide" if hide_window else ""
         # TODO: use this to hide window C:\msys64\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW64 /bin/bash -lc /c/path/to/your_program.exe
 
-        return BuilderBase.run(rf"""C:\msys64\msys2_shell.cmd -mingw64 -c "set -xe; {cmd};" """)
+        return self.run(rf"""{self.msys_path}\msys2_shell.cmd -mingw64 -c "set -xe; {cmd};" """)
